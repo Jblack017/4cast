8ef190802fc1a463698ee8b7ac7d82ea7e1f8d7f
Jblack017 <blackhamjoshua@comcast.net>
2021-07-21 11:14:31 -0600

    route update

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,6 +9,7 @@ const Projects = lazy(() => import("../pages/Projects"));
 
 const routes = [
   <Route path='/4cast' exact component={About} />,
+  <Route path='/4cast/about' exact component={About} />,
   <Route path='/4cast/stock_cast' exact component={StockCast} />,
   <Route path='/4cast/csv_cast' exact component={CsvCast} />,
   <Route path='/4cast/projects' exact component={Projects} />,
